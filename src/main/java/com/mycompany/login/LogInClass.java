f4cdd45ea5ab1538ad984acd8cdb1ba20d9f58d3
RC_Student_lab <RC_Student_lab@10.0.0.34>
2024-09-27 23:25:07 +0000

    done

--- src/main/java/com/mycompany/login/LogInClass.java
+++ src/main/java/com/mycompany/login/LogInClass.java
@@ -3,20 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.login;
-
-import java.util.HashMap;
-import java.util.Map;
 /**
  *
  * @author RC_Student_lab
  */
-public class LogInClass { //static map to store usernames and passwords for registered users
-    private static Map<String, String > userDatabase = new HashMap<>();
-
-    private String fname;
-    private String lname;
-    private String username;
-    private String password;
+public class LogInClass { 
+    public String fname;
+    public String lname;
+    public String username;
+    public String password;
 
     // Constructor to initialize
     public LogInClass (String fname, String lname, String username, String password) {
@@ -28,22 +23,26 @@ public class LogInClass { //static map to store usernames and passwords for regi
     //Check if username and password meets the requirements
     public static boolean checkUserName(String username) {
     // Check if the username has not more than of 5 characters long and contains any invalid characters (except underscore
-    if (username.length() > 5 || !username.contains("_")) {
-        return false;
+    if (username.length() >= 5 && !username.contains("_")) {
+        return true;
     }
     else {
-        return true;
+        return false;
     }
     }
  public static boolean checkPasswordComplexity (String password){
      if (password.length() < 8) {
                  return false;  // password must be at least 8 characters long
-                  }
+                 
+     }
 
         boolean hasCapitalLetter = false;
         boolean hasLowercaseLetter = false;
         boolean hasDigit = false;
         boolean hasSpecialCharacter = false;
+        
+
+        //Make if statements | Nest like if statements
 
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) {
@@ -64,58 +63,44 @@ public class LogInClass { //static map to store usernames and passwords for regi
         return hasCapitalLetter && hasLowercaseLetter && hasDigit && hasSpecialCharacter;
     }
                 
-           public static String registerUser(String usrname, String Password) { //register a username and password
-            userDatabase.put(username, password);
-            System.out.println("User registered successfully!");
-           username = "nale_"; // example username
+           public static void registerUser(String username, String Password) { //register a username and password
             if (checkUserName(username)){
                 System.out.println("Username succesfully captured");
          } else{
                 System.out.print("Username is not correctly formatted,please ensure than your username contains an underscore and is no more than 5 characters in length");
         // If all conditions are met, the username is valid 
             }
-       
-             password = "Naledi@16";  // example password
-        if (checkPasswordComplexity(password)) {
-            System.out.println("Password is not correctly formatted, please ensure that the password contains atleast 8 characters,a capital letter, a number and a special character");
-        } else {
+       if (checkPasswordComplexity(Password)) {
             System.out.println("Password succesfully captured");
-            return password;
+        } else {
+            System.out.println("Password is not correctly formatted, please ensure that the password contains atleast 8 characters,a capital letter, a number and a special character");
             }
            }
-           //Testing login
-            System.out.println(returnLogStatus(username, password));
-            
          //Check  username & display user friendly
-
-         public static boolean loginUser (String username, String password){
-         if (checkUserName){
+           
+         public static void loginUser (String username, String password){
+         
+         if (checkUserName (username)){
              System.out.print("Username successfully captured");
          } else{
              System.out.print("Username is not correctly formatted,please ensure than your username contains an underscore and is no more than 5 characters in length");
                      }
-         if (checkPasswordComplexity){
+         if (checkPasswordComplexity(password)){
+             System.out.print("Login succesful");
+         } else{
+             System.out.print("Login failed");
+                     }
          }
-         public static String returnLoginStatus (String username, String password){
+       public static String returnLoginStatus (String fname, String lname, String username, String password){
          //Check if the login details are correct
-         if (loginUser(username, password)){
-         }     
+        if ((checkUserName(username)) && (checkPasswordComplexity(password))){
+        System.out.println("Welcome " + fname + ", " + lname + ". It's great to see you again");
+        return "Login successful";
+        } else {
+            System.out.print("Username or password incorrect, please try again");
+             return "Login failed";
+       
 }
 }
 }     
-        
-           
-        
-   /**public String registerUser() {
-       if username && password = true;
-       System.out.print("Welcome" + fname + "," + lname + "it's great to see you again");
-       else System.out.print("Username or password incorrect, please try again")
-       
-        return ;
-       
-       
-       
-   }
-   
-}* **/
- 
+  
